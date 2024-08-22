9a09b2fd8547152dcb7a652fc8e81a2dc9a593bd
Shirin-Yan <shirin-yan@ShirinMBP.local>
2024-08-22 21:38:41 +0500

    Project created

---
+++ Scanner/ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerApp.swift
+//  Scanner
+//
+//  Created by Shirin-Yan on 22.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
